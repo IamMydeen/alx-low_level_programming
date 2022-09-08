6650cb84c41cf262f4b79120987b5d8cb6f7af1b
ajaka <ajakashamsudeen@gmail.com>
2022-09-08 16:59:02 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#!/bin/bash
 #include<stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	return (0);
 
 }
