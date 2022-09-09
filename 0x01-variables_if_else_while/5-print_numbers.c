6724cb68223b16d97f0e598f61a2cfde53e87eed
ajaka <ajakashamsudeen@gmail.com>
2022-09-09 14:52:07 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = '0';
+	int i = 0;
+	/* your code goes there */
+	while (i < 10)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
