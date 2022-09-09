8a8bed10c3091531aa651a0d952a63ef42d8b591
ajaka <ajakashamsudeen@gmail.com>
2022-09-09 13:55:47 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic
+ * Return: 0 
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
