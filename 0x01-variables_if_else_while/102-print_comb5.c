d07ecf00c5395d6c8bd80fa37fcab910ecc5d662
ajaka <ajakashamsudeen@gmail.com>
2022-09-09 16:31:11 -0700

    hot fix

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int last;
+	/* your code goes there */
+       	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			last = !(a == 98 && b == 99);
+			putchar(48 + a / 10);
+			putchar(48 + a % 10);
+			putchar(' ');
+			putchar(48 + b / 10);
+			putchar(48 + b % 10);
+			if (last)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
