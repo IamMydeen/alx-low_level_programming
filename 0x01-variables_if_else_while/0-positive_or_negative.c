3c018d5e63e7fe725a4948b1141677478821a1ee
ajaka <ajakashamsudeen@gmail.com>
2022-09-09 16:48:46 -0700

    first task:

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,14 @@
- <stdlib.h>
- <time.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - Entry point
+ * main - entry point
  *
  * Return: 0
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
