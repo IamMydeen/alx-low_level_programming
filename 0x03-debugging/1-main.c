2984d144c64760fd6af14feef616e6c5b6ca8b03
ajaka <ajakashamsudeen@gmail.com>
2022-09-16 07:23:14 -0700

    hot fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,9 +6,7 @@
 int main(void)
 {
 	int i;
-	
 	printf("Infinite loop incoming :(\n");
-	
 	i = 0;
 	/*
 	 * while (i < 10)
@@ -17,6 +15,5 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
